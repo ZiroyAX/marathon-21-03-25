a11f667e121fd365508e12c1e54c21bf69229ce5
ZiroyAX <Alex04061995@yandex.ru>
2021-08-26 11:56:04 +0400

    weatherforecast: finished styles

---
+++ js/weatherForecast.js
@@ -0,0 +1,7 @@
+const titleWeatherForecast = document.querySelector('.titleWeatherForecast');
+const btnWeek = titleWeatherForecast.children[1];
+const btnHour = titleWeatherForecast.children[2];
+
+btnWeek.style.paddingBottom = '9px';
+btnWeek.style.borderBottom = '3px solid #48484A';
+btnHour.style.color = '#ACACAC';
